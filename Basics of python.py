b6d65c7908aa37b75fc49af0359324a4ca92555f
Saeed-Jalal <syedsaeedjalal@gmail.com>
2018-02-05 02:34:26 +0200

    Basics of python

---
+++ Basics of python.py
@@ -0,0 +1,90 @@
+#Arithmetic Operators
+num1=60
+num2=45
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1//num2)
+print(num1%num2)
+print(7**3)
+
+#Assignemt Operator
+num3= num1+num2
+print(num3)
+num3+=num2
+print(num3)
+
+#Comparsion Operator
+print(num3>num2)
+print(num2==num3)
+print(num1!=num2)
+
+#Logical Operators
+A=True
+B=False
+print(A and B)
+print(A or B)
+print(not A)
+#Identity Operators
+X = 5
+X is 5
+X = 5
+X is not 5
+#Membership Operators
+ABC=[1,2,3,4,5,6,7,8,9]
+4 in ABC
+6 not in ABC
+
+
+#Python has two types of Data 1. Immutable (Numbers, Strings, Tuples) 2. Mutable (Lists, Dictionaries, Sets)
+#Numbers(Integer,Float and Complex)
+#Strings
+name1="My name is Saeed."
+name2="I am genius."
+#Concatenation
+print(name1+name2)
+#Repetition
+print(name1*2)
+#Slicing
+print(name2[2:7])
+#find()
+print(name1.find('S'))
+# replace()
+print(name2.replace("am","can be"))
+# split()
+job="Don"
+print(job.split(","))
+# count()
+print(name1.count("a"))
+#upper
+print(name1.upper())
+#max
+print(max(name2))
+#min
+str="HOME"
+print(min(str))
+#isalpha
+print(name1.isalpha())
+
+#Tuples
+myTup = ("Home","Office","Car","Computer")
+#Concatenation
+print(myTup+("Copter","Ship"))
+#Reprtition
+print(myTup*2)
+#Slicing
+print(myTup[1:3])
+
+
+
+
+
+#
+
+
+
+
+
+
+
+
